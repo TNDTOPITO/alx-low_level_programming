5b58d1baa81849af1f3f8cd116be092ee51efbd3
Thabiso <940303tabiso@gmail.com>
2022-06-09 11:37:15 -0700

    Task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -17,5 +22,6 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+void print_number(int n)
 
-
+#endif /* MAIN_H */
