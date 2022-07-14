b34307b521cf655cd96b9220a55223b865750b30
Thabiso <940303tabiso@gmail.com>
2022-07-14 10:45:12 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,12 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t nelem;
 
-	for (i = 0; h; i++)
+	nelem = 0;
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		nelem++;
 	}
-	return (i);
+	return (nelem);
 }
