9dee65a54088615d701d4a189365930aacdaebca
Thabiso <940303tabiso@gmail.com>
2022-07-21 05:48:44 -0700

    bit_manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,11 +10,14 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
+
 	if (index > 63)
 		return (-1);
+
 	m = 1 << index;
 
 	if (*n & m)
 		*n ^= m;
+
 	return (1);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from one number to another
  * @n: number one.
  * @m: number two.
  *
@@ -10,10 +11,12 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int nbits;
+
 	for (nbits = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			nbits++;
 	}
+
 	return (nbits);
 }
